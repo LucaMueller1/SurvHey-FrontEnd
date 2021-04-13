58a9fbbc66e3b274e465595eaa38f1df098c26e5
JiggoBonito <73295340+JiggoBonito@users.noreply.github.com>
2021-04-13 22:48:53 +0200

    Added Result Piechart

--- src/components/ResultChart.js
+++ src/components/ResultChart.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {AuthInterceptor} from '../services/AuthInterceptor';
 import {requestService} from '../services/requestService';
+import Chart from 'react-google-charts';
 
 
   
@@ -8,38 +9,37 @@ import {requestService} from '../services/requestService';
 export function ResultsChart(props) {
 
     const [results, setResults] = useState([]);
-    const [resultSum, setResultSum] = useState(0);
     const [id, setId] = useState(props.id)
 
     useEffect(() => {
         AuthInterceptor.intercept();
         requestService.getResults(id).then(res => {
-          console.log(res.data.choices);
-            setResults(Object.entries(res.data.choices));
+            setResults(res.data.choices);
         }
             
-            );
-        getResultSum();
-        console.log(results);
-        console.log(resultSum);
-        
+            );   
     }, []);
-      
-    const getResultSum =async () => {
-      await requestService.getAnalysis(id).then(res => {
-          setResultSum(res.data.amount);
-      }).catch( error => {
-          console.log(error);
-      });
-    }
 
+    let arr = [['AnswerOption', 'Submitted Answers'], ]
+    for(const [key, value] of Object.entries(results)) {
+        arr.push([key, value]);
+      }
+    console.log(arr);
+      
     return (
-        <div>
-            <p>Resultate + {id} </p>
-            <h3>{"AntwortContent" + results[0][0] }</h3>
-                <p>{"Anzahl:" + results[0][1]}</p>
-                <p>{"Gesamte Antworten:" + resultSum}</p>
-                <p>{"Antworten in Prozent:" + (results[0][1] / resultSum) * 100 + "%"}</p>
+        <div className="pieChartContainer" >
+            <Chart
+                className="resultPieChart"
+                width={'500px'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Results...</div>}
+                data={arr}
+                options={{
+                    title: 'Survey Results:',
+                }}
+                rootProps={{ 'data-testid': '1' }}
+            />
         </div>
     );
 }
